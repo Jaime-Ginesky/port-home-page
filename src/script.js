799d46f07584a3d5891d23fc27bf9a3c798e573d
Jaime-Ginesky <jaimelee79@gmail.com>
2023-10-24 12:17:59 -0700

    changed color palette and added features

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
+
+
 var circle = document.querySelector('.material-btn');
 var link = document.querySelector('.material-content').querySelectorAll('li');
 var ham = document.querySelector('.material-hamburger');
-var main = document.querySelector('main');
+var main = document.querySelector('section');
 var content = document.querySelector('.material-content');
 var win = window;
 
@@ -33,3 +35,37 @@ function closeMenu() {
 circle.addEventListener('click', openMenu, false);
 
 win.addEventListener('click', closeMenu, false);
+
+const toggleBtn = document.getElementById("toggle-btn");
+const theme = document.getElementById("theme");
+let darkMode = localStorage.getItem("dark-mode");
+
+
+
+// Function to create Dark/Light Mode button
+const enableDarkMode = () => {
+    theme.classList.add("dark-mode-theme");
+    toggleBtn.classList.remove("dark-mode-toggle");
+    localStorage.setItem("dark-mode", "enabled");
+    toggleBtn.innerHTML = `Light`;
+}
+const disableDarkMode = () => {
+    theme.classList.remove("dark-mode-theme");
+    toggleBtn.classList.add("dark-mode-toggle");
+    localStorage.setItem("dark-mode", "disabled");
+    toggleBtn.innerHTML = `Dark`;
+}
+
+if (darkMode === "enabled") {
+    enableDarkMode();
+}
+
+toggleBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    darkMode = localStorage.getItem("dark-mode");
+    if (darkMode === "disabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
