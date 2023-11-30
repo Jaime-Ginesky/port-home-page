a8f3c04fcbc535a7858fbc8b9a28cc620781c019
Jaime-Ginesky <jaimelee79@gmail.com>
2023-11-29 23:16:14 -0800

    restyled projects page

--- src/script-container.js
+++ src/script-container.js
@@ -2,7 +2,7 @@
  // Function to create a card for project menu
  function createCard(title, description, link) {
     const card = document.createElement('article');
-    card.classList.add('container','imgCard','contentC', 'content', 'card:hover','dark-mode-theme');
+    card.classList.add('projects-container','imgCard','contentC', 'content', 'card:hover','dark-mode-theme');
     // Add your desired card class here
     
     const contentElement = document.createElement('content');
